20746b55171446f06912af41d9660e408eeef0db
S CY <lol57@naver.com>
2020-06-11 14:16:02 +0900

    Upload init project

---
+++ SGPBicycleApp/SGPBicycleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SGPBicycleApp
+//
+//  Created by kpugame on 2020/06/11.
+//  Copyright © 2020 SONCHAEYOUNG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
